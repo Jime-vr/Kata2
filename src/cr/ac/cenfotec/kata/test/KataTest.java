1220c6a181c43a6a09c8764507797eabba3de77d
DESKTOP-LJ4L6HP\jimev <jimevegar@gmail.com>
2018-06-09 19:20:50 -0600

    Refactoring

--- src/cr/ac/cenfotec/kata/test/KataTest.java
+++ src/cr/ac/cenfotec/kata/test/KataTest.java
@@ -11,62 +11,62 @@ public class KataTest {
 	Kata kat = new Kata();
 	
 	@Test
-	public void test1() {
+	public void primoDe1() {
 		assertEquals("[]", kat.generate(1));
 	}
 	
 	@Test 
-	public void test2() {
+	public void primoDe2() {
 		assertEquals("2 [2]", kat.generate(2));
 	}
 	
 	@Test
-	public void test3() {
+	public void primoDe3() {
 		assertEquals("3 [3]", kat.generate(3));
 	}
 	
 	@Test 
-	public void test4() {
+	public void primoDe4() {
 		assertEquals("4 [2,2]", kat.generate(4));
 	}
 
 	@Test
-	public void test5() {
+	public void primoDe5() {
 		assertEquals("5 [5]", kat.generate(5));
 	}
 	
 	@Test
-	public void test6() {
+	public void primoDe6() {
 		assertEquals("6 [2,3]", kat.generate(6));
 	}
 	
 	@Test
-	public void test7() {
+	public void primoDe7() {
 		assertEquals("7 [7]", kat.generate(7));
 	}
 	
 	@Test
-	public void test8() {
+	public void primoDe8() {
 		assertEquals("8 [2,2,2]", kat.generate(8));
 	}
 	
 	@Test
-	public void test9() {
+	public void primoDe9() {
 		assertEquals("9 [3,3]", kat.generate(9));
 	}
 	
 	@Test 
-	public void test10() {
+	public void primoDe36() {
 		assertEquals("36 [2,2,3,3]", kat.generate(36));
 	}
 	
 	@Test 
-	public void test11() {
+	public void primoDe42() {
 		assertEquals("42 [2,3,7]", kat.generate(42));
 	}
 	
 	@Test
-	public void test12() {
+	public void primoDe4620() {
 		assertEquals("4620 [2,2,3,5,7,11]", kat.generate(4620));
 	}
 }
